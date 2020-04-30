4f88c0a4c63723b1cc0f8660c64dec8243a79fa0
AndrewCopeland <andcope1995@gmail.com>
2020-04-30 15:20:30 -0400

    add endpoints for retrieve secret, set secret and list resources

--- ConjurClient/Endpoints.cs
+++ ConjurClient/Endpoints.cs
@@ -14,13 +14,32 @@ namespace ConjurClient
 
         public String Authenticate()
         {
-            string baseAuthnUrl = _config.AuthnUrl.Replace(_config.ApplianceUrl, "") + "/" + _config.Account;
+            string baseAuthnUrl = _config.AuthnUrl.Replace(_config.ApplianceUrl, "");
             return String.Format($"{baseAuthnUrl}/{_config.Account}/{WebUtility.UrlEncode(_config.Username)}/authenticate");
         }
 
         public String RetrieveSecret(string variableId)
         {
-            return String.Format($"secrets/{_config.Account}/variable/{WebUtility.UrlEncode(variableId)}");
+            return String.Format($"/secrets/{_config.Account}/variable/{WebUtility.UrlEncode(variableId)}");
+        }
+
+        public String AddSecret(string variableId)
+        {
+            return String.Format($"/secrets/{_config.Account}/variable/{WebUtility.UrlEncode(variableId)}");
+        }
+
+        public String ListResources(string kind, string search)
+        {
+            string resourceEndpoint = String.Format($"/resources/{_config.Account}");
+            if (!String.IsNullOrWhiteSpace(kind))
+            {
+                resourceEndpoint += $"/{kind}";
+            }
+            if (!String.IsNullOrWhiteSpace(search))
+            {
+                resourceEndpoint += $"?search={search}";
+            }
+            return resourceEndpoint;
         }
 
         public String Info()
