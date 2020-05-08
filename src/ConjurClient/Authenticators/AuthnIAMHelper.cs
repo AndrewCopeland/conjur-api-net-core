384eb29459d14b5bbe2b4cd49d700b675e7cfe76
AndrewCopeland <andcope1995@gmail.com>
2020-05-08 14:58:30 -0400

    Return a SecureString

--- src/ConjurClient/Authenticators/AuthnIAMHelper.cs
+++ src/ConjurClient/Authenticators/AuthnIAMHelper.cs
@@ -105,7 +105,7 @@ namespace ConjurClient.Authenticators
         }
 
         // Is used for unit tests
-        public static String GetAuthenticationRequest(String accessKey, String secretKey, String sessionToken, DateTime now)
+        public static SecureString GetAuthenticationRequest(String accessKey, String secretKey, String sessionToken, DateTime now)
         {
             String amzdate = GetAmzDate(now);
             String datestamp = GetDate(now);
@@ -121,10 +121,10 @@ namespace ConjurClient.Authenticators
             byte[] signingKey = GetSignatureKey(secretKey, datestamp, REGION, SERVICE);
             signature = SignString(stringToSign, signingKey);
             String authorizationHeader = GetAuthorizationHeader(accessKey, GetCredentialScope(datestamp), SIGNED_HEADERS, signature);
-            return HeaderAsJsonString(amzdate, sessionToken, payloadHash, authorizationHeader);
+            return Utilities.AsSecureString(HeaderAsJsonString(amzdate, sessionToken, payloadHash, authorizationHeader));
         }
 
-        public static String GetAuthenticationRequest(String accessKey, String secretKey, String sessionToken)
+        public static SecureString GetAuthenticationRequest(String accessKey, String secretKey, String sessionToken)
         {
             return GetAuthenticationRequest(accessKey, secretKey, sessionToken, DateTime.UtcNow);
         }
